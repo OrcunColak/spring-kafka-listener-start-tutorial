48967abec3f6b05a2e3d280efebc3b62f2318a90
Orçun Çolak <orcun.colak@hazelcast.com>
2024-10-02 13:07:33 +0300

    move configuration to application.properties

--- src/main/java/com/colak/springtutorial/config/KafkaConsumerConfig.java
+++ src/main/java/com/colak/springtutorial/config/KafkaConsumerConfig.java
@@ -1,7 +1,7 @@
 package com.colak.springtutorial.config;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -10,23 +10,29 @@ import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableKafka
+@RequiredArgsConstructor
 public class KafkaConsumerConfig {
 
+    private final KafkaProperties kafkaProperties;
+
+    // protected Map<String, Object> consumerConfig() {
+    //     Map<String, Object> propsMap = new HashMap<>();
+    //     propsMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    //     propsMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+    //
+    //     propsMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+    //
+    //     propsMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    //     propsMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+    //     return propsMap;
+    // }
+
     protected Map<String, Object> consumerConfig() {
-        Map<String, Object> propsMap = new HashMap<>();
-        propsMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        propsMap.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-
-        propsMap.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-
-        propsMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        propsMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return propsMap;
+        return kafkaProperties.buildConsumerProperties(null);
     }
 
     @Bean
